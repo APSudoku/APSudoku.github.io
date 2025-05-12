d6267f932a383419e67e1d3c5de7f03922862814
Emily Venezia <emilygamergirl99@gmail.com>
2025-05-11 21:11:18 -0400

    v3.1.4 -> v3.1.5

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747007877|1424549813';
+const CACHE_VERSION = '1747012269|463456873';
 /** @type {string} */
 const CACHE_PREFIX = 'APSudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
